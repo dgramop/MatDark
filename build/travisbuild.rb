3da40b08bd7a6eeffcb5934007fc9e2f24264c81
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2017-03-26 22:37:29 -0400

    Im done

--- build/travisbuild.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-result = `sass matdark.scss matdark.css`
-raise result unless $?.to_i == 0
-raise "When compiled this should spit some supa-hot CSS" unless File.exists?('matdark.css')
-puts "Regular compile worked successfully"
