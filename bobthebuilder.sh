a037f050dc962970c5a9d1a2eabbdf78492d4790
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2017-03-30 14:43:50 -0400

    Fixed
    Works on all machines, I have css-invert as a bin, but its OK,

--- bobthebuilder.sh
+++ bobthebuilder.sh
@@ -2,4 +2,4 @@
 scss main.scss build/matdark.css;
 cp ./matdark.js ./build/matdark.js;
 scss matlight.scss build/matlight.css;
-css-invert build/matlight.css build/matlight.css;
+./build/css-invert build/matlight.css build/matlight.css;
