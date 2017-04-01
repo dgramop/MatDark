589e7d3aad337d066ee6e554c505aacf07e25374
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2017-03-31 21:33:14 -0400

    Update bobthebuilder.sh

--- build/bobthebuilder.sh
+++ build/bobthebuilder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scss main.scss build/matdark.css;
-cp ./matdark.js ./build/matdark.js;
-scss matlight.scss build/matlight.css;
-./build/css-invert build/matlight.css build/matlight.css;
+sass ../main.scss matdark.css;
+cp ./matdark.js ./matdark.js;
+sass ../matlight.scss matlight.css;
+./css-invert build/matlight.css build/matlight.css;
